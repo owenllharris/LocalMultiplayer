95197b9366e5f42d4534f3e1894257feb61918fb
owenllharris <owenllharris@gmail.com>
2015-09-19 15:17:20 +0100

    fregre

---
+++ LocalMultiplayer/Assets/LocalCameraEnable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LocalCameraEnable : NetworkBehaviour {
+
+    public GameObject cam;
+    public GameObject camHolder;
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            cam = GameObject.Find("PlayerCamera");
+            cam.transform.position = camHolder.transform.position;
+            cam.transform.parent = camHolder.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
